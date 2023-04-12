b9523e082886593c62aa7b717993a6a729e950e6
Anthony Audrain <anthonyaud78@gmail.com>
2023-04-12 17:51:23 +0200

    amélioration des routes

---
+++ softDesk/authentification/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from authentification import views
+
+app_name = "authentification"
+
+urlpatterns = [
+    path("token/auth/", TokenObtainPairView.as_view(), name="token"),
+    path("token/refresh/auth/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/signup/", views.signup, name="signup"),
+    path("auth/login/", views.login, name="login")
+]
