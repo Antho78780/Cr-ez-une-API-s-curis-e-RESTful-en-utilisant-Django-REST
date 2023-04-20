681f8752d33a74bb14564da0ec5ec02dc8082fdb
Anthony Audrain <anthonyaud78@gmail.com>
2023-04-21 01:45:31 +0200

    mise à jour du endpoint signup

--- softDesk/authentification/urls.py
+++ softDesk/authentification/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import routers
 
-from authentification import views
+from authentification.views import RegisterViews
 
 app_name = "authentification"
 
 urlpatterns = [
     path("token/refresh/auth/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("auth/signup/", views.signup, name="signup"),
-    path("auth/login/", TokenObtainPairView.as_view(), name="login")
+    path("auth/signup/", RegisterViews.as_view(), name="signup"),
+    path("auth/login/", TokenObtainPairView.as_view(), name="login"),
 ]
