122417bc6102b67d32906bdb506306454fdc3a9e
Anthony Audrain <anthonyaud78@gmail.com>
2023-04-21 01:43:58 +0200

    ajout de class RegisterSerializer

---
+++ softDesk/authentification/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from .models import User
+
+
+class registerSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "email", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username = validated_data["username"],
+            first_name = validated_data["first_name"],
+            last_name = validated_data["last_name"],
+            email = validated_data["email"],
+            password = validated_data["password"]
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
