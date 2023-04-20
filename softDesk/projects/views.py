b91a2a8f61d0485c963ff0541306f2cdd9f70884
Anthony Audrain <anthonyaud78@gmail.com>
2023-04-21 01:50:05 +0200

    ajout la possibilité de créer un projet en ajoutant le créateur du projet + filtrer l'autheur du projet

--- softDesk/projects/views.py
+++ softDesk/projects/views.py
@@ -1,33 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from .models import Comment, Project, Issue
-from .serializers import CommentSerializer, ProjectSerializer, IssuesSerializer
-
-
-class CommentViewset(ModelViewSet):
-    serializer_class = CommentSerializer
-
-    def get_queryset(self):
-       return Comment.objects.all()
+from .models import  Project
+from .serializers import ProjectSerializer
 
 
 class ProjectViewset(ModelViewSet):
     serializer_class = ProjectSerializer
     permission_classes = [IsAuthenticated]
     
+
     def get_queryset(self):
-        queryset = Project.objects.all()
+        queryset = Project.objects.filter(author_user=self.request.user)
         return queryset
     
     def perform_create(self, serializer):
         return serializer.save(author_user=self.request.user)
-
-class IssuesViewset(ModelViewSet):
-    serializer_class = IssuesSerializer
-
-    def get_queryset(self):
-        return Issue.objects.all()
-
-
+    
