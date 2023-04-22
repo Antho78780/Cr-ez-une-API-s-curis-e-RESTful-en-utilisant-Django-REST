75639c176d739db7aed53feea6ac8894427359fc
Anthony Audrain <anthonyaud78@gmail.com>
2023-04-22 13:29:59 +0200

    mise à jour de l'endpoint api

--- softDesk/softDesk/urls.py
+++ softDesk/softDesk/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include("authentification.urls")),
-    path('', include("projects.urls"))
+    path('auth/', include("authentification.urls")),
+    path("api/", include("projects.urls"))
 ]
