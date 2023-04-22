3629a83a63bfee221379e9f886754d95412ecb3b
Anthony Audrain <anthonyaud78@gmail.com>
2023-04-22 13:39:24 +0200

    mise à jour des endpoints

--- softDesk/authentification/urls.py
+++ softDesk/authentification/urls.py
@@ -3,11 +3,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import routers
 
 from authentification.views import RegisterViews
+from authentification import views
 
 app_name = "authentification"
 
 urlpatterns = [
     path("token/refresh/auth/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("auth/signup/", RegisterViews.as_view(), name="signup"),
-    path("auth/login/", TokenObtainPairView.as_view(), name="login"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("signup/", RegisterViews.as_view(), name="signup"),
 ]
