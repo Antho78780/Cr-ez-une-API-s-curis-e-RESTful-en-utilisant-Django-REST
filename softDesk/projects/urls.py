b48b455473855f38ee675b84c3d9a201a3ee0b10
Anthony Audrain <anthonyaud78@gmail.com>
2023-04-24 16:15:01 +0200

    ajout de urlpatterns avec les urls

--- softDesk/projects/urls.py
+++ softDesk/projects/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
-from .views import ProjectViewset, ContributorViewSet
+from .views import ProjectViewset, ContributorViewSet, ContributorDestroyViewSet, IssuesViewSet, IssueUpdateDestroyViewSet
 from rest_framework import routers
 
 app_name = "projects"
 
 router = routers.DefaultRouter()
 router.register(r"projects", ProjectViewset, basename="projects")
-router.register(r"projects/1/users", ContributorViewSet, basename="contributors")
 
 
-urlpatterns = router.urls  
+urlpatterns = [
+    path("projects/<project_id>/users/", ContributorViewSet.as_view(), name="contributor"),
+    path("projects/<project_id>/users/<user_id>/", ContributorDestroyViewSet.as_view(), name="destroy_user"),
+    path("projects/<project_id>/issues/", IssuesViewSet.as_view(), name="issues"),
+    path("projects/<project_id>/issues/<issue_id>/", IssueUpdateDestroyViewSet.as_view(), name="update_destroy_issue")
+]
+urlpatterns += router.urls  
